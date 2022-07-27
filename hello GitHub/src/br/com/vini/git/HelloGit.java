51a200f8b7f5ee3802735e26601faec03e432c12
viniciuscm <vinideuso.19@gmail.com>
2022-07-27 09:51:58 -0400

    adicionada mais uma linha exemplar

--- hello GitHub/src/br/com/vini/git/HelloGit.java
+++ hello GitHub/src/br/com/vini/git/HelloGit.java
@@ -15,6 +15,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git");
+		System.out.println("Vinicius Celani Mendonça");
 
 	}
 
